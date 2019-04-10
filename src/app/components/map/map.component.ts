1470be55b5d6304072c0fe82d5f45c02f194cb16
Sisou <Combes.sylvain30@gmail.com>
2019-04-10 18:17:44 +0200

    Revert "map gpx"
    This reverts commit 1d801a6a695d403d561eecdbeda64d6e625838a1.

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -3,16 +3,16 @@ import Map from 'ol/Map.js';
 import Overlay from 'ol/Overlay.js';
 import View from 'ol/View.js';
 import {toStringHDMS} from 'ol/coordinate.js';
+import TileLayer from 'ol/layer/Tile.js';
 import {fromLonLat, toLonLat} from 'ol/proj.js';
 import OSM from 'ol/source/OSM.js';
+
 import Feature from 'ol/Feature.js';
 import Point from 'ol/geom/Point.js';
+import {Tile, Vector as VectorLayer} from 'ol/layer.js';
 import TileJSON from 'ol/source/TileJSON.js';
 import VectorSource from 'ol/source/Vector.js';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer.js';
-import GPX from 'ol/format/GPX.js';
-import {Circle as CircleStyle, Fill, Stroke, Style,Icon} from 'ol/style.js';
-import { stringify } from '@angular/compiler/src/util';
+import {Icon, Style} from 'ol/style.js';
 
 
 
@@ -29,147 +29,149 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
 
-     this.initilizeMap();
-     var ListPoint = this.CreatePoint();
-     this.setpoint(ListPoint);
-     this.LoadGpx();
+    this.initilizeMap();
+    this.setpointtree();
+    this.view.setCenter([-3.683333, 40.4]);
+
   }
 
+// SetPoint(){
 
-LoadGpx(){
-  
+//   var pos = fromLonLat([4.083333, 44.133333],'EPSG:4326');
+
+//   // Vienna marker
+//   var marker = new Overlay({
+//     position: pos,
+//     positioning: 'center-center',
+//     element: document.getElementById('marker'),
+//     stopEvent: false
+//   });
+//   this.map.addOverlay(marker);
+
+//   // Vienna label
+//   var vienna = new Overlay({
+//     position: pos,
+//     element: document.getElementById('vienna')
+//   });
+//   this.map.addOverlay(vienna);
+
+//   // Popup showing the position the user clicked
+//   var popup = new Overlay({
+//     element: document.getElementById('popup')
+//   });
+//   this.map.addOverlay(popup);
+
+//   this.map.on('click', function(evt) {
+//     var element = popup.getElement();
+//     var coordinate = evt.coordinate;
+//     var hdms = toStringHDMS(toLonLat(coordinate));
+
+    
+//     element.popover('destroy');
+//     popup.setPosition(coordinate);
+//     element.popover({
+//       placement: 'top',
+//       animation: false,
+//       html: true,
+//       content: '<p>The location you clicked was:</p><code>' + hdms + '</code>'
+//     });
+//     element.popover('show');
+//   });
+
+// }
+
+// SetpointTwo(){
+//   var rome = new Feature({
+//     geometry: new Point(fromLonLat([12.5, 41.9]))
+//   });
+
+//   var london = new Feature({
+//     geometry: new Point(fromLonLat([-0.12755, 51.507222]))
+//   });
+
+//   var madrid = new Feature({
+//     geometry: new Point(fromLonLat([-3.683333, 40.4]))
+//   });
+
+//   rome.setStyle(new Style({
+//     image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+//       anchorXUnits: 'fraction',
+//       anchorYUnits: 'fraction',
+//       src: '../../../assets/mountains.png'
+//     }))
+//   }));
+
+//   london.setStyle(new Style({
+//     image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+//       color: '#4271AE',
+//       crossOrigin: 'anonymous',
+//       src: 'E:\Moutain Sports\Moutain Sports\src\assets\mountains.png'
+//     }))
+//   }));
+
+//   madrid.setStyle(new Style({
+//     image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
+//       color: [113, 140, 0],
+//       crossOrigin: 'anonymous',
+//       src: 'E:\Moutain Sports\Moutain Sports\src\assets\mountains.png'
+//     }))
+//   }));
 
 
-  var styletoto = {
-    'Point': new Style({
-      image: new CircleStyle({
-        fill: new Fill({
-          color: 'rgba(255,255,0,0.4)'
-        }),
-        radius: 5,
-        stroke: new Stroke({
-          color: '#ff0',
-          width: 1
-        })
-      })
-    }),
-    'LineString': new Style({
-      stroke: new Stroke({
-        color: '#f00',
-        width: 3
-      })
-    }),
-    'MultiLineString': new Style({
-      stroke: new Stroke({
-        color: '#0f0',
-        width: 3
-      })
-    })
-  };
+//   var vectorSource = new VectorSource({
+//     features: [rome, london, madrid]
+//   });
 
-  var vectorGpx = new VectorLayer({
-    source: new VectorSource({
-      url: '../../../assets/bike.gpx',
-      format: new GPX()
-    }),
-    style: function(feature) {
-      return styletoto['point'];
-    }
-  });
+//   var vectorLayer = new VectorLayer({
+//     source: vectorSource
+//   });
 
-  this.map.addLayer(vectorGpx);
+//   var rasterLayer = new TileLayer({
+//     source: new TileJSON({
+//       url: 'https://api.tiles.mapbox.com/v3/mapbox.geography-class.json?secure',
+//       crossOrigin: ''
+//     })
+//   });
 
-  var displayFeatureInfo = function(pixel) {
-    var features = [];
-    this.map.forEachFeatureAtPixel(pixel, function(feature) {
-      features.push(feature);
-    });
-    if (features.length > 0) {
-      var info = [];
-      var i, ii;
-      for (i = 0, ii = features.length; i < ii; ++i) {
-        info.push(features[i].get('desc'));
-        console.log(stringify("info:"+info));
-      }
-      document.getElementById('info').innerHTML = info.join(', ') || '(unknown)';
-      this.map.getTarget().style.cursor = 'pointer';
-    } else {
-      document.getElementById('info').innerHTML = '&nbsp;';
-      this.map.getTarget().style.cursor = '';
-    }
-  };
-  
+//   var map = new Map({
+//     layers: [rasterLayer, vectorLayer],
+//     target: document.getElementById('map'),
+//     view: new View({
+//       center: fromLonLat([2.896372, 44.60240]),
+//       zoom: 3
+//     })
+//   });
+// }
+
+
+setpointtree(){
+  var markers = new VectorLayer.Markers( "Markers" );
+this.map.addLayer(markers);
+
+var size = new Style.Size(21,25);
+var offset = new Style.Pixel(-(size.w/2), -size.h);
+var icon = new Icon('http://www.openlayers.org/dev/img/marker.png', size, offset);
+markers.addMarker(new VectorLayer.Markers(new fromLonLat.LonLat(4.083333, 44.133333),icon));
+markers.addMarker(new VectorLayer.Markers(new fromLonLat.LonLat(4.083333, 44.133333),icon.clone()));
 }
 
-  setpoint(ListPoint){
-
-    var vectorLayer = new VectorLayer({
-      source: ListPoint
-    });
-    this.map.addLayer(vectorLayer);
-  }
-
-   CreatePoint(){
-
-    var LesSaisies = new Feature({
-      geometry: new Point(fromLonLat([6.538260, 45.758362]))
-    });
-
-    var AlpesHuez = new Feature({
-      geometry: new Point(fromLonLat([6.064360, 45.090510]))
-    });
-
-    var Chamrousse = new Feature({
-      geometry: new Point(fromLonLat([5.876630, 45.126460]))
-    });
-
-    LesSaisies.setStyle(new Style({
-      image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-        color: '#8959A8',
-        crossOrigin: 'anonymous',
-        src: '../../../assets/mountains (1).png',
-        scale: 0.1
-      }))
-    }));
-
-    AlpesHuez.setStyle(new Style({
-      image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-        color: '#4271AE',
-        crossOrigin: 'anonymous',
-        src: '../../../assets/mountains (1).png',
-        scale: 0.1
-      }))
-    }));
-
-    Chamrousse.setStyle(new Style({
-      image: new Icon(/** @type {module:ol/style/Icon~Options} */ ({
-        color: [113, 140, 0],
-        crossOrigin: 'anonymous',
-        src: '../../../assets/mountains (1).png',
-        scale: 0.1
-      }))
-    }));
-
-    var vectorSource = new VectorSource({
-      features: [LesSaisies, AlpesHuez, Chamrousse]
-    });
-
-    return vectorSource;
-  }
 
   initilizeMap() {
 
-    var rasterLayer = new TileLayer({
+    var layer = new TileLayer({
       source: new OSM()
     });
 
-    this.map = new Map({
-      layers: [rasterLayer],
-      target: document.getElementById('map'),
-      view: new View({
-        center: fromLonLat([5.876016, 45.376810]),
-        zoom: 7
-      })
+    this.view = new View({
+      center: [0, 0],
+      projection : 'EPSG:4326',// longitude,lattitude mondiale
+      zoom: 15
     });
+
+    this.map = new Map({
+      layers: [layer],
+      target: 'map',
+      view: this.view
+      });
   }
 }
