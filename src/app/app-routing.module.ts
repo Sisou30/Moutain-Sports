a8d52c7b672e51ca625f2a082c7c688c7c19d410
Sisou <Combes.sylvain30@gmail.com>
2020-02-26 13:13:30 +0100

    feat : add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
 import { HomeComponent } from './components/home/home.component';
+import { ParcoursComponent } from './components/parcours/parcours.component';
+import { LoadParcourComponent } from './components/load-parcour/load-parcour.component';
+import { CreateParcoursComponent } from './components/create-parcours/create-parcours.component';
+import { SearchParcourComponent } from './components/search-parcour/search-parcour.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
+        children: [
+          {
+            path: '',
+            component: ParcoursComponent,
+          },
+          {
+            path: 'Parcours',
+            component: ParcoursComponent,
+          },
+          {
+            path: 'LoadParcour',
+            component: LoadParcourComponent,
+          },
+          {
+            path: 'CreateParcours',
+            component: CreateParcoursComponent,
+          },
+          {
+            path: 'SearchParcour',
+            component: SearchParcourComponent,
+          }
+        ]
     }
 ];
 
